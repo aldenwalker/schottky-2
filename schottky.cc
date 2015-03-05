e26874c5ec88170f933811e3c8477b18d744bac5
Alden Walker <akwalker@math.uchicago.edu>
2015-03-04 18:46:48 -0600

    4d stuff

--- schottky.cc
+++ schottky.cc
@@ -1,14 +1,11 @@
 /*	schottky.cc
 
-	Version 0.01 March 15 2014
-
-	Copyright Danny Calegari
+	Copyright Danny Calegari and Alden Walker
 
 	released under the terms of the GNU GPL, version 3.0
 	see attached licence for more details
 */
 
-// standard libraries to include
 
 #include <iostream>
 #include <cstdlib>
@@ -38,23 +35,59 @@ int main(int argc, char *argv[]) {
       G.find_traps_along_circle_in_window(2, false);
       return 0;
     }
+    
+    if (std::string(argv[1]) == "-4db" && argc == 7) {
+      cpx center( atof(argv[2]), atof(argv[3]) );
+      double rad = atof(argv[4]);
+      double step = atof(argv[5]);
+      int depth = atof(argv[6]);
+      int num_boxes = (2.0*rad)/step;
+      std::vector<std::vector<std::vector<std::vector<bool> > > > res(0);
+      for (double a1=center.real()-rad; a1 < center.real()+rad; a1 += step) {
+        res.push_back(std::vector<std::vector<std::vector<bool> > >(0));
+        for (double b1=center.imag()-rad; b1 < center.imag()+rad; b1 += step) {
+        res.back().push_back(std::vector<std::vector<bool> >(0));
+          for (double a2=center.real()-rad; a2 < center.real()+rad; a2 += step) {
+          res.back().back().push_back(std::vector<bool>(0));
+            for (double b2=center.imag()-rad; b2 < center.imag()+rad; b2 += step) {
+              ifs G( cpx(a1,b1), cpx(a2,b2), 1, 0);
+              int diff;
+              //std::cout << "Testing " << cpx(a1,b1) << " " << cpx(a2,b2) << "\n";
+              res.back().back().back().push_back(G.is_connected(depth, diff));
+            }
+          }
+        }
+      }
+      for (int i=0; i<(int)res.size(); ++i) {
+        for (int j=0; j<(int)res[i].size(); ++j) {
+          for (int k=0; k<(int)res[i][j].size(); ++k) {
+            for (int ell=0; ell<(int)res[i][j][k].size(); ++ell) {
+              std::cout << "{" << i << "," << j << "," << k << "," << ell << "," << res[i][j][k][ell] << "},";
+            }
+          }
+        }
+      }
+      return 0;
+    }
+    
+              
   }
   
   //std::cout << sizeof(long long int) <<"\n";
   
-//  std::cout << "enter 'i' for IFS or 'm' for mandelbrot (n for new GUI):";
-//  std::cin >> c;
-//   if (c == 'n') {
-//     IFSGui G;
-//     G.launch(BOTH, cpx(-0.58, 0.33));
-//   } else {
-//     mode = (c=='i' ? 0 : 1);
-//     ifs G(cos(TWOPI/3.0)+I*sin(TWOPI/3.0), 0.5, w, mode);      // default value : Sierpinski triangle
-//     G.user_interface();
-//   }
+ std::cout << "enter 'i' for IFS or 'm' for mandelbrot (n for new GUI):";
+ std::cin >> c;
+  if (c == 'n') {
+    IFSGui G;
+    G.launch(BOTH, cpx(-0.58, 0.33));
+  } else {
+    mode = (c=='i' ? 0 : 1);
+    ifs G(cos(TWOPI/3.0)+I*sin(TWOPI/3.0), 0.5, w, mode);      // default value : Sierpinski triangle
+    G.user_interface();
+  }
   
-  IFSGui G;
-  G.launch(BOTH, cpx(-0.58, 0.33));
+  //IFSGui G;
+  //G.launch(BOTH, cpx(-0.58, 0.33));
   
   return 0;
 }
